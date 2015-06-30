cc73b6e252e297f852665b1ad6bfa437b3c0bf56
Bryan Davis <bryan@stax.io>
2015-06-30 10:43:01 -0700

    Update main.go

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bytes"
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
+	"os"
 	"strconv"
+	"strings"
 )
 
 type Node struct {
@@ -15,102 +18,79 @@ type Node struct {
 }
 
 func main() {
-	// filename := "simple.xml"
-	filename := "xml/simple.xml"
-
-	b, _ := ioutil.ReadFile(filename)
-	j := ImportXML(b)
-	ioutil.WriteFile("json/simple.json", j, 0700)
+	n := Nodify("xml/complex.xml")
+	n.Write("json/complex.json", 0700)
+	n.Pretty(0)
 }
 
-func Walk(n Node, indent int) interface{} {
-
-	var tabs string
-	for i := 0; i < indent; i++ {
-		tabs += "\t"
-	}
-
+func Walk(n Node) interface{} {
 	if len(n.Nodes) < 1 {
-		// m[n.XMLName.Local] = string(n.Content)
 		return string(n.Content)
 	} else {
-		// m[n.XMLName.Local] = make([]interface{}, len(n.Nodes))
-		// m[n.XMLName.Local] = make(map[int]interface{})
-
-		// var x []interface{}
 		x := make(map[string][]interface{})
 		y := make(map[string]interface{})
 
 		for _, v := range n.Nodes {
-			// x = append(x, Walk(v, indent+1))
-
-			x[v.XMLName.Local] = append(x[v.XMLName.Local], Walk(v, indent+1))
-
-			// if len(v.Nodes) < 1 {
-			// 	fmt.Printf("%s(%d) %s == content node (%s)\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)), string(v.Content))
-			// 	// m[n.XMLName.Local] = append(m[n.XMLName.Local].([]interface{}), string(v.Content))
-
-			// } else {
-			// 	fmt.Printf("%s(%d) %s == parent node\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)))
-			// 	// m[n.XMLName.Local] = append(m[n.XMLName.Local].([]interface{}), Walk(v, indent+1))
-			// 	m[n.XMLName.Local] = Walk(v, indent+1)
-
-			// }
+			x[v.XMLName.Local] = append(x[v.XMLName.Local], Walk(v))
 		}
 
 		for k, v := range x {
 			if len(x[k]) == 1 {
 				switch v[0].(type) {
-				case int, int32, int64:
-					i, _ := strconv.Atoi(v[0].(string))
-					y[k] = i //v[0].(int)
 				case float32, float64:
 					f, _ := strconv.ParseFloat(v[0].(string), 64)
-
 					y[k] = f // v[0].(float64)
 				case bool:
 					y[k] = v[0].(bool)
 				default:
 					y[k] = v[0]
 				}
-
 			} else {
 				y[k] = v
 			}
 		}
-
 		return y
 	}
-
-	// for k, v := range n.Nodes {
-	// 	if len(v.Nodes) < 1 {
-	// 		fmt.Printf("%s(%d) %s == content node (%s)\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)), string(v.Content))
-	// 		m[v.XMLName.Local] = string(v.Content)
-	// 	} else {
-	// 		fmt.Printf("%s(%d) %s == parent node\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)))
-	// 		m[v.XMLName.Local] = Walk(v, indent+1)
-	// 	}
-	// }
-
-	// return m
 }
 
-func ImportXML(data []byte) []byte {
+func Nodify(filename string) Node {
+	data, _ := ioutil.ReadFile(filename)
 	buf := bytes.NewBuffer(data)
 	dec := xml.NewDecoder(buf)
 
 	var n Node
 	dec.Decode(&n)
 
+	return n
+}
+
+func (n Node) Import() []byte {
 	var y []interface{}
 	for _, v := range n.Nodes {
-		y = append(y, Walk(v, 0))
-
+		y = append(y, Walk(v))
 	}
 
-	// m := Walk(n, 0)
 	b, _ := json.Marshal(y)
-	// fmt.Println(string(b))
 	return b
+}
 
+func (n Node) Pretty(indent int) {
+	var tabs string
+	for i := 0; i < indent; i++ {
+		tabs += "\t"
+	}
+
+	for k, v := range n.Nodes {
+		if len(v.Nodes) < 1 {
+			fmt.Printf("%s(%d) %s == content node (%s)\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)), string(v.Content))
+		} else {
+			fmt.Printf("%s(%d) %s == parent node\n", tabs, k, strings.Title(strings.ToLower(v.XMLName.Local)))
+			v.Pretty(indent + 1)
+		}
+	}
+}
+
+func (n Node) Write(name string, perm os.FileMode) {
+	j := n.Import()
+	ioutil.WriteFile(name, j, perm)
 }
